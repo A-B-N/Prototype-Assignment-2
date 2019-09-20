7fd2217000f83d998fe51e373c0fe9760e1810cf
Pooja Ramaswamy <poojaramaswamy94@gmail.com>
2019-09-20 15:13:09 -0700

    Final Changes

--- controllers/main.js
+++ controllers/main.js
@@ -1,10 +1,9 @@
-var lineReader= require("line-reader");
+var lineReader = require("line-reader");
 var registeredUsers = [];
 
-function sendPage(fileName, res)
-{
+function sendPage(fileName, res) {
     var html = '';
-    
+
     // Read the file one line at a time.
     lineReader.eachLine(fileName,
         /**
@@ -14,69 +13,59 @@ function sendPage(fileName, res)
          * @param line the line read from the file.
          * @param last set to true after the last line.
          */
-        function(line, last)
-        {
+        function(line, last) {
             html += line + '\n';
 
-            if (last) 
-            { 
+            if (last) {
                 res.send(html);
-                return false; 
-            }
-            else
-            {
+                return false;
+            } else {
                 return true;
             }
         });
 }
-function getCredit(req)
-{
+
+function getCredit(req) {
     var creditScore = req.param('creditScore');
-   
-    
+
+
     return creditScore;
 }
 
-module.exports.index = function(req, res, next) 
-{
-   res.render('index', {title:'Authentication Demo'});
-   console.log('Cookies: ', req.cookies);
+module.exports.index = function(req, res, next)  {  
+    res.render('index', { title: 'Authentication Demo' });  
+    console.log('Cookies: ', req.cookies);
 };
 
-module.exports.get_register = function(req, res)
-{
-    res.render('register',
-               { message: "Please register!" });
+module.exports.get_register = function(req, res) {
+    res.render('register', { message: "Please register!" });
 };
-module.exports.post_register = function(req, res)
-{
-    if(!req.body.username || !req.body.password)
-    {
+module.exports.post_register = function(req, res) {
+    if (!req.body.username || !req.body.password) {
         res.status("400");
         res.send("Invalid details!");
-    } 
-    else 
-    {
+    } else {
         // Create an array of users with matching usernames.
-        var matches = registeredUsers.filter(function(user)
-                      {
-                          return user.username === req.body.username;
-                      });
-        
+        var matches = registeredUsers.filter(function(user) {
+            return user.username === req.body.username;
+        });
+
         // If there is a match, the user has already registered.
-        if (matches.length > 0)
-        {
-            res.render('register', {message: "User already registered!"});
+        if (matches.length > 0) {
+            res.render('register', { message: "User already registered!" });
         }
-        
+
         // Register a new user.
-        else
-        {
-            var newUser = { username: req.body.username, 
-                            password: req.body.password };
+        else {
+            var newUser = {
+                username: req.body.username,
+                password: req.body.password
+            };
             registeredUsers.push(newUser);
-            console.log("New user:"); console.log(newUser);
-            console.log("Registered users:"); console.log(registeredUsers);
+            console.log("New user:");
+            console.log(newUser);
+            console.log("Registered users:");
+            console.log(registeredUsers);
             res.redirect('/login');
         }
     }
@@ -85,53 +74,46 @@ module.exports.post_register = function(req, res)
 /*
  * GET login page.
  */
-module.exports.get_login = function(req, res)
-{
-   res.render('login', { message: "Please log in!" });
+module.exports.get_login = function(req, res) {
+    res.render('login', { message: "Please log in!" });
 };
-module.exports.post_login = function(req, res)
-{
-    console.log("Registered users:"); console.log(registeredUsers);
+module.exports.post_login = function(req, res) {
+    console.log("Registered users:");
+    console.log(registeredUsers);
     console.log("Logging in: " + req.body.username + "/" + req.body.password);
-    
+
     // Create an array of users with matching credentials.
-    var matches = registeredUsers.filter(function(user)
-                  {
-                      return    (user.username === req.body.username) 
-                             && (user.password === req.body.password);
-                  });
-    
-    console.log("Matching credentials: "); console.log(matches);
-    
-    if (matches.length === 0)
-    {
-        res.render('login', {message: "Invalid credentials!"});
-    }
-    else
-    {
+    var matches = registeredUsers.filter(function(user) {
+        return (user.username === req.body.username) &&
+            (user.password === req.body.password);
+    });
+
+    console.log("Matching credentials: ");
+    console.log(matches);
+
+    if (matches.length === 0) {
+        res.render('login', { message: "Invalid credentials!" });
+    } else {
         // The user is logged in for this session.
         req.session.user = matches[0];
-        console.log("Sucessfully logged in:"); 
+        console.log("Sucessfully logged in:");
         console.log(req.session.user.username);
-        
+
         res.render('form');
     }
 };
 
 
-module.exports.get_custDetails = function(req, res)
-{
-   sendPage('form.html',res);
+module.exports.get_custDetails = function(req, res) {
+    sendPage('form.html', res);
 };
-module.exports.post_custDetails =function(req,res)
-{
-   var creditScore= getCredit(req);
-   if(creditScore<600){
-     
-      res.render('loandeclined');
-   }
-   else{
-     
-      res.render('loanApproved');
-   }
+module.exports.post_custDetails = function(req, res) {
+    var creditScore = getCredit(req);
+    if (creditScore < 600) {
+
+        res.render('loandeclined');
+    } else {
+
+        res.render('loanApproved');
+    }
 };
