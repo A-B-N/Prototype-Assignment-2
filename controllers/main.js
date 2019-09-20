c5e1c719ab5002edc94a5bd7c657850aeb57617d
Ajith Balaji <ajithbalajiun@gmail.com>
2019-09-19 23:31:18 -0700

    updating the changes

--- controllers/main.js
+++ controllers/main.js
@@ -1,3 +1,70 @@
+var lineReader= require("line-reader");
+function sendPage(fileName, result)
+{
+    var html = '';
+    
+    // Read the file one line at a time.
+    lineReader.eachLine(fileName,
+        /**
+         * Append each line to string html.
+         * Send the contents of html to the client
+         * after the last line has been read.
+         * @param line the line read from the file.
+         * @param last set to true after the last line.
+         */
+        function(line, last)
+        {
+            html += line + '\n';
+
+            if (last) 
+            { 
+                result.send(html);
+                return false; 
+            }
+            else
+            {
+                return true;
+            }
+        });
+}
+function sendBodyReject(text, result)
+{
+    var html = '<!DOCTYPE html>\n'
+        + '<html lang="en-US">\n'
+        + '<head>\n'
+        + '    <meta charset="UTF-8">\n'
+        + '    <title>Form Examples</title>\n'
+        + '</head>\n'
+        + '<body>\n'
+        + '    ' + text + '\n'  // insert the body text
+        + '</body>\n'
+        + '</html>\n';
+    
+    res.send(html);    
+}
+function sendBodyApprove(text, result)
+{
+    var html = '<!DOCTYPE html>\n'
+        + '<html lang="en-US">\n'
+        + '<head>\n'
+        + '    <meta charset="UTF-8">\n'
+        + '    <title>Form Examples</title>\n'
+        + '</head>\n'
+        + '<body>\n'
+        + '    ' + text + '\n'  // insert the body text
+        + '</body>\n'
+        + '</html>\n';
+    
+    res.send(html);    
+}
+function getCredit(request)
+{
+    var creditScore = request.param('creditScore');
+   
+    
+    return creditScore;
+}
+
 module.exports.index = function(req, res, next) 
 {
    res.render('index', {title:'Authentication Demo'});
@@ -16,4 +83,20 @@ module.exports.get_register = function(req, res)
 module.exports.get_login = function(req, res)
 {
    res.render('login', { message: "Please log in!" });
+};
+module.exports.get_custDetails = function(req, res)
+{
+   sendPage('form.html',result);
+};
+module.exports.post_custDetails =function(req,res)
+{
+   var creditScore= getcreditScore(req);
+   if(creditScore<600){
+      var text= 'Credit score is below the threshold';
+      sendBodyReject(text,result);
+   }
+   else{
+      var text= 'Credit score meets the requirement';
+      sendBodyApprove(text,result);
+   }
 };
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var ctrlMain = require("../controllers/main");
+var multer = require("multer");
+var bodyParser=require("body-parser")
 /*
  * GET home page.
  */
 router.get('/', ctrlMain.index);
 router.get('/register', ctrlMain.get_register);
 router.get('/login', ctrlMain.get_login);
+router.get('/form',ctrlMain.get_custDetails);
+router.post('/form',ctrlMain.post_custDetails);
 module.exports = router;
